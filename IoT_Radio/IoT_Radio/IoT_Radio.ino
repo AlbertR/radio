fb98f655e2389cbda01f1527573e8823ed39bdf3
AlbertR <igor.ushakov.cg@gmail.com>
2021-07-05 17:23:31 +0300

    Add wifi module functions (GetWiFiList, GetRadioStationList, ConnectSSID, DisconnectFromSSID)

--- IoT_Radio/IoT_Radio/IoT_Radio.ino
+++ IoT_Radio/IoT_Radio/IoT_Radio.ino
@@ -12,21 +12,26 @@
 #define RX 10
 #define TX 11
 
+/*
+0 - Get WIFI IP info
+1 - List WiFi
+2 - Connect to SSID
+3 - Disconnect from SSID
+4 -
+
+Each command must have a range depending on 1024/(number of command)
+*/
+#define WIFI_IP 0
+#define WIFI_LIST_SSID 1
+#define WIFI_CONNECT 2
+#define WIFI_DISCONNECT 3
+
 SoftwareSerial wifi(RX, TX);
 
 int status = 0;
 int max_value;
 int current_value;
 bool BUTTON_PRESSED = false;
-/* 
-0 - Get WIFI IP info
-1 - List WiFi 
-2 - Connect to SSID
-3 - Disconnect from SSID
-4 - 
-
-Each command must have a range depending on 1024/(number of command)
-*/ 
 int current_selected_command = -1;
 
 void setup() {
@@ -49,11 +54,11 @@ void loop() {
 	control_level = analogRead(analog_CONTROL_PIN); // Может быть до 1023;
 
 	if ( control_level > 0 && control_level < 246) {
-		if (current_selected_command != 0) {
+		if (current_selected_command != WIFI_IP) {
 			Serial.print("Level ");
 			Serial.println(control_level);
 			Serial.println("Get WIFI IP info");
-			current_selected_command = 0;
+			current_selected_command = WIFI_IP;
 		}
 		if ((digitalRead(BUTTON_PIN) == HIGH) && !BUTTON_PRESSED) {
 			SendCommand("0");
@@ -61,11 +66,11 @@ void loop() {
 		}
 	}
 	else if (control_level > 267 && control_level < 502) {
-		if (current_selected_command != 1) {
+		if (current_selected_command != WIFI_LIST_SSID) {
 			Serial.print("Level ");
 			Serial.println(control_level);
 			Serial.println("List WIFI Stations");
-			current_selected_command = 1;
+			current_selected_command = WIFI_LIST_SSID;
 		}
 		if ((digitalRead(BUTTON_PIN) == HIGH) && !BUTTON_PRESSED) {
 			SendCommand("1");
@@ -73,11 +78,11 @@ void loop() {
 		}
 	}
 	else if (control_level > 523 && control_level < 758) {
-		if (current_selected_command != 2) {
+		if (current_selected_command != WIFI_CONNECT) {
 			Serial.print("Level ");
 			Serial.println(control_level);
 			Serial.println("Connect to SSID");
-			current_selected_command = 2;
+			current_selected_command = WIFI_CONNECT;
 		}
 		if ((digitalRead(BUTTON_PIN) == HIGH) && !BUTTON_PRESSED) {
 			SendCommand("2");
@@ -85,11 +90,11 @@ void loop() {
 		}
 	}
 	else if (control_level > 779 && control_level < 1023) {
-		if (current_selected_command != 3) {
+		if (current_selected_command != WIFI_DISCONNECT) {
 			Serial.print("Level ");
 			Serial.println(control_level);
 			Serial.println("Disconnect from SSID");
-			current_selected_command = 3;
+			current_selected_command = WIFI_DISCONNECT;
 		}
 		if ((digitalRead(BUTTON_PIN) == HIGH) && !BUTTON_PRESSED) {
 			SendCommand("3");
--- WiFiSetup/WiFiSetup/WiFiSetup.ino
+++ WiFiSetup/WiFiSetup/WiFiSetup.ino
@@ -16,6 +16,15 @@
 const char * SSID = WIFISSID;
 const char * PW = WIFISSIDPW;
 
+// Number of wifi networks
+int scanNetworkResult = 0;
+String ssid;
+uint8_t encryptionType;
+int32_t RSSI;
+uint8_t* BSSID;
+int32_t channel;
+bool isHidden;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
 
@@ -34,31 +43,37 @@ void loop() {
 	if (Serial.available() > 0) {
 		switch (Serial.read())
 		{
-		case '0':
+		case '0': // WIFI status
 			switch (WiFi.status()) {
-			case '0':
+			case WL_IDLE_STATUS:
 				Serial.print("IDLE");
 				break;
-			case '1':
+			case WL_NO_SSID_AVAIL:
 				Serial.print("NO SSID AVAILABLE");
 				break;
 			case WL_CONNECTED:
 				Serial.println("CONNECTED");
 				Serial.print(WiFi.localIP());
 				break;
-			case '4':
+			case WL_CONNECT_FAILED:
 				Serial.print("CONNECT FAILED");
 				break;
-			case '6':
+			case WL_WRONG_PASSWORD:
 				Serial.print("CONNECT WRONG PASSWORD");
 				break;
-			case '7':
+			case WL_DISCONNECTED:
 				Serial.print("DISCONNECTED");
 				break;
 			default:
 				Serial.print("Unknown");
 				break;
 			}
+		case '1': // List WiFi stations
+			GetWiFiList();
+			break;
+		case '2': // Connect to SSID
+			break;
+		case '3': // Dissconnect from SSID
 			break;
 		default:
 			break;
@@ -68,17 +83,36 @@ void loop() {
 }
 
 void GetWiFiList() {
-
+	if (WiFi.status() == WL_CONNECTED) {
+		scanNetworkResult = WiFi.scanNetworks(false, true);
+		if (scanNetworkResult == 0) {
+			Serial.print("No wifi networks found");
+		}
+		else if (scanNetworkResult > 0) {
+			for (int8_t i = 0; i < scanNetworkResult; i++) {
+				WiFi.getNetworkInfo(i, ssid, encryptionType, RSSI, BSSID, channel, isHidden);
+				Serial.printf("%s\n", ssid.c_str());
+			}
+			delay(0);
+		}
+		else {
+			Serial.print("Scan network failed");
+		}
+	}
 }
 
 void GetRadioStationList() {
-
+	// Update station list
+	// Get from site
 }
 
-void ConnectToSSID() {
-
+void ConnectToSSID(String SSID, String Password) {
+	WiFi.begin(SSID, Password);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+	}
 }
 
 void DisconnectFromSSID() {
-
+	WiFi.disconnect();
 }
